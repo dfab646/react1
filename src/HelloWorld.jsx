162bfc801bf94b2a228ed607aacfa89bcdcb66cd
X <x@Xs-MacBook-Air.local>
2019-11-11 19:08:06 -0500

    Messsage1

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => (
+    <div>Hello World</div>
+);
+
+const Security = () => (
+    <img src="https://www.openbsd.org/images/tshirt-28_front.gif"/> 
+);
+
+export default Security;
